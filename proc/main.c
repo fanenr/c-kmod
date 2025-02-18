63198ed91264c9c6f48d4db0fbc126180d8b9712
Arthur63 <arthur63@163.com>
2025-02-18 19:41:18 +0800

    add hello_write

--- proc/main.c
+++ proc/main.c
@@ -1,15 +1,21 @@
 #include <linux/module.h>
 #include <linux/proc_fs.h>
 
+#define DATA_MAX 1024
 #define PROC_NAME "hello"
 
+static char data[DATA_MAX];
+static size_t data_size = 0;
 static struct proc_dir_entry *hello_proc;
 
 static ssize_t hello_read (struct file *filp, char __user *buff, size_t size,
                            loff_t *offp);
+static ssize_t hello_write (struct file *filp, const char __user *buff,
+                            size_t size, loff_t *offp);
 
 static const struct proc_ops hello_fops = {
   .proc_read = hello_read,
+  .proc_write = hello_write,
 };
 
 static int __init
@@ -56,6 +62,23 @@ hello_read (struct file *filp, char __user *buff, size_t size, loff_t *offp)
     }
 
   return ret;
+}
+
+static ssize_t
+hello_write (struct file *filp, const char __user *buff, size_t size,
+             loff_t *offp)
+{
+  size = min (size, DATA_MAX - 1);
+
+  if (copy_from_user (data, buff, size))
+    return -EFAULT;
+  data[size] = 0;
+
+  *offp += size;
+  data_size = size;
+  pr_info ("procfile write %s\n", data);
+
+  return size;
 }
 
 module_init (hello_init);
